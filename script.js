c517e5079d9decb0e16e7f6058e10db00067a5bd
Risvisan <69704449+Risvisan@users.noreply.github.com>
2026-03-10 11:37:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 onload = () => {
   document.body.classList.remove("container");
+
 };
+document.addEventListener("DOMContentLoaded", function(){
+
+    document.addEventListener("click", iniciarMusica, { once: true });
+    document.addEventListener("touchstart", iniciarMusica, { once: true });
+
+    function iniciarMusica(){
+
+        const audio = document.getElementById("musica");
+
+        if(audio){
+            audio.volume = 0;
+            audio.play();
+
+            let volumen = 0;
+
+            const fade = setInterval(()=>{
+
+                if(volumen < 1){
+                    volumen += 0.02;
+                    audio.volume = volumen;
+                }else{
+                    clearInterval(fade);
+                }
+
+            },100);
+        }
+    }
+
+});
